8c703fca34400bae7a284bb787209492646267b1
Tom Draper <tomjdraper1@gmail.com>
2025-11-26 11:18:15 +0000

    fix: CDN URL path to public folder

--- lib/utils/cdn.ts
+++ lib/utils/cdn.ts
@@ -1,7 +1,7 @@
 
 export const withCDN = (path: string) => {
     if (process.env.NODE_ENV === 'production') {
-        return `https://cdn.jsdelivr.net/gh/tom-draper/uk-data-atlas${path}`;
+        return `https://cdn.jsdelivr.net/gh/tom-draper/uk-data-atlas/public${path}`;
     }
     return path;
 };
