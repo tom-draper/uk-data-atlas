85a353c7bf7b340f1c25d0b99296fd9f17c181d7
Tom Draper <tomjdraper1@gmail.com>
2026-05-29 15:06:23 +0100

    feat: dark mode datasets page

--- components/displays/LoadingDisplay.tsx
+++ components/displays/LoadingDisplay.tsx
@@ -6,7 +6,7 @@ export default function LoadingDisplay() {
 	return (
 		<div className="absolute inset-0 flex items-center justify-center bg-[#fafafa] z-10">
 			<div className="text-sm text-gray-500">
-				<Image src="/UK-2.png" alt="" width={300} height={200} className="h-[200px] w-auto mb-10 mr-4" />
+				<Image src="/UK-2.png" alt="" width={300} height={200} className="h-[200px] mb-10 mr-4" style={{ width: "auto" }} />
 			</div>
 		</div>
 	);
