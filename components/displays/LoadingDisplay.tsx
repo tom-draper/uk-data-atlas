46c473ee91fdc1a5f53755edef59f4ffe6085735
Tom Draper <tomjdraper1@gmail.com>
2026-08-26 16:37:46 +0100

    fix: preserve loading image proportions

--- components/displays/LoadingDisplay.tsx
+++ components/displays/LoadingDisplay.tsx
@@ -6,7 +6,14 @@ export default function LoadingDisplay() {
 	return (
 		<div className="absolute inset-0 flex items-center justify-center bg-[#fafafa] z-10">
 			<div className="text-sm text-gray-500">
-				<Image src="/UK-2.png" alt="" width={300} height={200} className="mb-10 mr-4" style={{ height: "200px", width: "auto" }} loading="eager" />
+				<Image
+					src="/UK-2.png"
+					alt=""
+					width={141}
+					height={200}
+					className="mb-10 mr-4"
+					loading="eager"
+				/>
 			</div>
 		</div>
 	);
