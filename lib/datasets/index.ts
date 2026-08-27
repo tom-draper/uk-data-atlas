c0cabcfbcb7449e75ee6c624ab21e722f2dca327
Tom Draper <tomjdraper1@gmail.com>
2026-08-27 15:38:39 +0100

    build: generate dataset registry

--- lib/datasets/index.ts
+++ lib/datasets/index.ts
@@ -1,12 +1,2 @@
-import { childPovertyDefinition } from "./childPoverty";
-import { fuelPovertyDefinition } from "./fuelPoverty";
-import { homelessnessDefinition } from "./homelessness";
-import type { ScalarDatasetDefinition } from "./types";
-
-export const SCALAR_DATASET_DEFINITIONS: readonly ScalarDatasetDefinition[] = [
-	childPovertyDefinition,
-	homelessnessDefinition,
-	fuelPovertyDefinition,
-] as const;
-
+export { SCALAR_DATASET_DEFINITIONS } from "./generated";
 export type { ScalarDatasetDefinition } from "./types";
