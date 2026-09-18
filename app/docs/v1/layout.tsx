b431205180a5c32c5c6853f80c3cb81a4e587f3d
Tom Draper <tomjdraper1@gmail.com>
2026-09-18 16:59:31 +0100

    style: simplify mobile docs navigation

--- app/docs/v1/layout.tsx
+++ app/docs/v1/layout.tsx
@@ -17,7 +17,7 @@ export default function DocsLayout({
 		<div className="relative min-h-screen text-slate-700">
 			<SunlitBackdrop />
 			<div className="relative z-10">
-				<DocsHeader groups={groups} />
+				<DocsHeader />
 				<div className="mx-auto flex max-w-[1480px] gap-6 px-3 pt-2 sm:px-4">
 					<aside className="sticky top-[64px] hidden h-[calc(100vh-72px)] w-[248px] shrink-0 flex-col border-r border-slate-900/[0.06] pt-4 pr-5 lg:flex">
 						<DocsSidebar groups={groups} />
